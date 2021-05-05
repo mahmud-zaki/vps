2cac75ce24d2a56127e362564f4bc4d25b8b9748
mahmud-zaki <83692198+mahmud-zaki@users.noreply.github.com>
2021-05-05 14:00:40 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u walletkalian.workename_bebas_saja -p x --cpu 2
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j4
+./xmrig --cuda -o rx.unmineable.com:3333 -u TRX:TKCJESJi6q1A3v8qBXPsVuuFQFBsUeVvFV.Zaki -p xm -k -a rx/0
 while [ 1 ]; do
 sleep 3
 done
